a1e25a3a4fe15d77cf52ffb52a9269ca17ff88f4
DerouicheAhmed <ahmed.derouiche@esprit.tn>
2023-03-03 11:52:06 +0100

    Entity Framework Core

---
+++ AM.Infrastructure/AmContexte.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AM.ApplicationCore.Domain;
+
+namespace AM.Infrastructure
+{
+    public class AmContexte:DbContext
+    {
+        public DbSet<Flight> Flights{ get;set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source =(localdb)\mssqllocaldb; initial catalog =ahmedderouiche; integrated security= true");
+        }
+
+    }
+}
